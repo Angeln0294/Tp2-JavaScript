f4c553902c94f1bfc8dfbca32f55f29a8b710051
Angel Nader <angeln0294@gmail.com>
2026-03-16 13:07:44 -0300

    correccion del ejercicio13

--- js/13-ejercicio.js
+++
@@ -1,9 +0,0 @@
-const texto = prompt("Escribi tu texto aqui")
-
-if(texto && isNaN(texto)){
-    const textoMayuscula = texto.toUpperCase();
-    document.body.innerHTML = `<p>Tu texto en mayuscula</p>${textoMayuscula}`
-    isNaN
-}else{
-    document.body.innerHTML = "<h2>No ingresaste ningun texto.</h2>"
-}
